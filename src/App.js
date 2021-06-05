cdb9ace8729a67e8cf898930d2aa9479616ddadf
Yvan Sraka <yvan@sraka.xyz>
2021-06-05 15:07:23 +0200

    Fresh start ...

--- src/App.js
+++
@@ -1,7 +0,0 @@
-// Material Design elements
-import { MDCRipple } from "@material/ripple";
-import { MDCTextField } from "@material/textfield";
-
-// Material Design elements
-new MDCRipple(document.querySelector(".mdc-button"));
-new MDCTextField(document.querySelector(".mdc-text-field"));
